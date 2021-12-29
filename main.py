efe95ab86c56fa2da29b3b94fcc435f78dd12fea
GayThomas <96692099+GayThomas@users.noreply.github.com>
2021-12-29 22:29:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ def mainDownload():
     subredditList = ['SUBREDDIT NAME', 'SUBREDDIT NAME', 'SUBREDDIT_NAME', 'SUBREDDIT_NAME', 'SUBREDDIT_NAME', 'SUBREDDIT_NAME', 'SUBREDDIT_NAME', 'SUBREDDIT_NAME']
     for subreddit in subredditList:
         redditscrapper.redditScraper(subreddit, amountOfPosts=25,topOfWhat='week',Path='PATH')
-        print("-----END-----")
+    print("-----END-----")
 if __name__ == "__main__":
     mainDownload()
